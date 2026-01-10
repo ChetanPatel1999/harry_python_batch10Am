ee44a04378240c128fa396c0063074b1f6ae8d2b
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2026-01-10 11:18:16 +0530

    1/5

---
+++ 11_ascii_value.py
@@ -0,0 +1,4 @@
+#  Write a program print ASCII value of any char.
+character=input("enter a char : ")
+print("charcter is : ",character)
+print("unii code  is : ",ord(character))
---
+++ 12.upper_charToLower.py
@@ -0,0 +1,4 @@
+#  Write a program to convert upper char to lower char. 
+upperChar=input("enter a char : ")
+lowerChar=chr(ord(upperChar)+32)
+print("lower character = ",lowerChar)
