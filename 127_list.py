d6a4787d83785bd528ab934dfd009a62c66affd3
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-02-24 11:23:37 +0530

    24/02

---
+++ 127_list.py
@@ -0,0 +1,16 @@
+#example of list
+#how to iterate(visit) list element 
+l1=[12,34,56,78,90]
+#using for loop
+# for ele in l1:
+#     print(ele)
+
+#using range function
+# for i in range(len(l1)):
+#     print(l1[i])
+
+#using while loop
+i=0
+while i< len(l1):
+    print(l1[i])
+    i+=1
---
+++ 128_list_constructor.py
@@ -0,0 +1,23 @@
+# The list() Constructor  
+# It is also possible to use the list() constructor when creating a new list. 
+
+# l1=list((12,34,56,78))
+# print(l1)
+# print(type(l1))
+
+# l1=list({12,34,56,78})
+# print(l1)
+# print(type(l1))
+
+# l1=list("hello")
+# print(l1)
+# print(type(l1))
+
+# l1=list(range(1,11))
+# print(l1)
+# print(type(l1))
+
+d1={12:100,13:200,14:500}
+l1=list(d1)
+print(l1)
+print(type(l1))
