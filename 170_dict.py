24c5ffe75977a8ef6229897417ad26a206c04bde
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-03-16 11:19:02 +0530

    16/03

--- 170_dict.py
+++ 170_dict.py
@@ -27,20 +27,26 @@ d1 = {
     70: "seventy",
     80: "eighty",
     90: "ninety",
-    100: "one hundred",
-    200: "two hundred"
+    100: "hundred",
+    1000: "thousand",
 }
 
-num=int(input("enter a num : "))#56
+num=int(input("enter a num : "))#100
 if num in d1:
     print(d1[num])
 else:
-    if len(str(num))==2:
+    if len(str(num))==2: # 78
         f=num//10 * 10
         s=num%10
         print(f"{d1[f]} {d1[s]}")
-    else: #567
-        f=num//100 * 100
-        s=num//10%10 *10
-        t=num%10
-        print(f"{d1[f]} {d1[s]} {d1[t]}")
+    elif len(str(num))==3:  # 567
+        f=num//100 # 5
+        s=num//10%10 *10  # 60
+        t=num%10 # 7
+        print(f"{d1[f]} hundred {d1[s]} {d1[t]}")
+    elif len(str(num))==4:  # 5678
+        f=num//1000 # 5
+        s=num//100%10  # 6
+        t=num//10%10*10 # 70
+        fo=num%10
+        print(f"{d1[f]} thousand {d1[s]} hundred {d1[t]}  {d1[fo]}")    
---
+++ 171_set.py
@@ -0,0 +1,4 @@
+s1={34,56,33,8,6,89}
+
+for item in s1:
+    print(item)
